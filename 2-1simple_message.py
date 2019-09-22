479b9b9d228cd05e45a07a74a8ed2aacd35f7d74
spdev <spdev@npdevs-MacBook-Pro.local>
2019-09-22 10:54:26 -0400

    first day

---
+++ 2-1simple_message.py
@@ -0,0 +1,3 @@
+#Assign a message to a variable, and then print that message
+message = "This is my message"
+print(message)
---
+++ 2-2simple_messages.py
@@ -0,0 +1,5 @@
+#Assign a message to a variable, and print that message. Then change the value of the variable to a new message, and print the new message
+message = "First message"
+print(message)
+message = "Second message"
+print(message)
