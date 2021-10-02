f622814460fec9957ca2ce357fe253011cf010c2
SeanPDev <SeanPDev@gmail.com>
2021-10-02 12:18:06 -0400

    Brushing up python skills

---
+++ data_types.py
@@ -0,0 +1,32 @@
+import math
+
+# Fundamental Data Types
+#int 
+#float 
+#bool
+#str
+#list
+#tuple
+#set
+#dict
+
+#Classes -> custom types
+
+#Specialized Data Types
+#Modules
+
+#None #Absence of value
+
+#int
+print(2 + 4)
+print(2 - 4)
+print(2 * 4)
+print(2 / 4)
+ 
+
+ #Math functions
+import math
+a= math.sqrt(100)
+b= math.sqrt(15)
+print(a)
+print(b)
